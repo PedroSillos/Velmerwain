2918a93370ca99d48895f97b9075e6ce63a5bf76
PedroSillos <pedro.sillos@gmail.com>
2025-08-20 20:04:30 -0300

    Add "matchs" and fix KDA

--- load_match_statistics.py
+++ load_match_statistics.py
@@ -5,15 +5,18 @@ import pandas as pd
 def loadMatchStatsTable(stage_file_name,stats_file_name):
     df_match_stage = pd.read_csv(stage_file_name)
 
+    df_match_stage['gameVersion'] = df_match_stage['gameVersion'].str.split('.').str[:2].str.join('.')
+
     key_columns = ["puuid", "gameVersion", "individualPosition", "championName"]
     
     df_calc_stats = df_match_stage.groupby(key_columns).agg(
-        winRate=("win", "mean"),
+        matchs=("championName", lambda x: x.count()),
+        winRate=("win", lambda x: f"{x.mean() * 100:.2f}%"),
         KDA=(
               "kills",
               lambda x: (
                 (df_match_stage.loc[x.index, "kills"] + df_match_stage.loc[x.index, "assists"])
-                / df_match_stage.loc[x.index, "deaths"]).sum()
+                / df_match_stage.loc[x.index, "deaths"]).mean()
         ),
         averageKills=("kills", "mean"),
         averageDeaths=("deaths", "mean"),
@@ -27,10 +30,7 @@ def loadMatchStatsTable(stage_file_name,stats_file_name):
 
     df_calc_stats["modifiedAt"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    if not os.path.exists(stats_file_name):
-        df_calc_stats.to_csv(stats_file_name, index=False)
-    else:
-        df_file = pd.read_csv(stats_file_name)
+    df_calc_stats.to_csv(stats_file_name, index=False)
 
 if __name__ == "__main__":
     stage_file_name = "stage_match.csv"
