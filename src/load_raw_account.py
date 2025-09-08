c48039799f5ec18f55c9c5b8c3a8eb3ce1f64c2f
PedroSillos <pedro.sillos@gmail.com>
2025-09-08 11:42:43 -0300

    Add logic for when json exists

--- src/load_raw_account.py
+++ src/load_raw_account.py
@@ -28,15 +28,36 @@ def get_account_data(game_name: str, tag_line: str, api_key: str):
     return response.json()
 
 def save_account_data(file_path: str, account_data: dict):
-    account_data['createdOn'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    account_data['modifiedOn'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
     os.makedirs(file_path.replace("/raw_account.json",""), exist_ok=True)
     
     if os.path.isfile(file_path):
-        print('in progress')
-    else:
+        users = []
+        updated = False
+        
+        with open(file_path, "r", encoding="utf-8") as file:
+            users = json.load(file)
+        
+        for user in users:
+            if user["puuid"] == account_data["puuid"]:
+                user["gameName"] = account_data["gameName"]
+                user["tagLine"] = account_data["tagLine"]
+                user["modifiedOn"] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+                updated = True
+        
+        if not updated:
+            users.append(account_data)
+        
         with open(file_path, "w", encoding="utf-8") as file:
-            json.dump(account_data, file)
+            json.dump(users, file, indent=4)
+
+    else:
+        users = []
+        users.append(account_data)
+
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(users, file, indent=4)
 
 if __name__ == "__main__":
     game_name, tag_line, api_key = get_args()
