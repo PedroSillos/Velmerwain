6aa84d75f98fb7b5b1e010bb325180c73c95c85b
PedroSillos <pedro.sillos@gmail.com>
2025-09-12 19:58:15 -0300

    Load player info

--- src/load_silver_player.py
+++ src/load_silver_player.py
@@ -1,7 +1,7 @@
 import os
-import requests
 from datetime import datetime
 import json
+import csv
 
 def get_file_path(file_name: str, file_dir: str):
     current_path = os.path.abspath(__file__)
@@ -24,38 +24,62 @@ def get_puuids_and_accounts():
 
     return puuids, accounts
 
-def get_account_regions(puuids: list, accounts: list):
+def get_account_regions(puuids: list, players: list):
     file_path = get_file_path(file_name="raw_account_region.json", file_dir="bronze")
 
     with open(file_path, "r", encoding="utf-8") as file:
         account_regions = json.load(file)
     
     for puuid in puuids:
-        for account in accounts:
-            if puuid == account["puuid"]:
+        for player in players:
+            if puuid == player["puuid"]:
                 for account_region in account_regions:
                     if puuid == account_region["puuid"]:
-                        account["game"] = account_region["game"]
-                        account["region"] = account_region["region"]
+                        player["game"] = account_region["game"]
+                        player["region"] = account_region["region"]
                         break
                 break
 
-    return accounts
+    return players
 
-def save_summoners(file_path: str, summoners: dict):
-    for summoner in summoners:
-        summoner['modifiedOn'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+def get_summoners(puuids: list, players: list):
+    file_path = get_file_path(file_name="raw_summoner.json", file_dir="bronze")
 
-    os.makedirs(file_path.replace("/raw_summoner.json",""), exist_ok=True)
+    with open(file_path, "r", encoding="utf-8") as file:
+        summoners = json.load(file)
+    
+    for puuid in puuids:
+        for player in players:
+            if puuid == player["puuid"]:
+                for summoner in summoners:
+                    if puuid == summoner["puuid"]:
+                        player["profileIconId"] = summoner["profileIconId"]
+                        player["revisionDate"] = summoner["revisionDate"]
+                        player["summonerLevel"] = summoner["summonerLevel"]
+                        break
+                break
 
-    with open(file_path, "w", encoding="utf-8") as file:
-        json.dump(summoners, file, indent=4)
+    return players
+
+def save_players(file_path: str, players: dict):
+    for player in players:
+        player['sourceModifiedOn'] = player['modifiedOn']
+        player['modifiedOn'] = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+    os.makedirs(file_path.replace("/silver_player.csv",""), exist_ok=True)
+
+    field_names = players[0].keys()
+    
+    with open(file_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
 
 if __name__ == "__main__":
-    puuids, accounts = get_puuids_and_accounts()
+    puuids, players = get_puuids_and_accounts()
 
-    account_regions = get_account_regions(puuids, accounts)
+    players = get_account_regions(puuids, players)
 
-    #file_path = get_file_path(file_name="raw_summoner.json", file_dir="bronze")
+    players = get_summoners(puuids, players)
 
-    #save_summoners(file_path, summoners)
+    file_path = get_file_path(file_name="silver_player.csv", file_dir="silver")
+
+    save_players(file_path, players)
