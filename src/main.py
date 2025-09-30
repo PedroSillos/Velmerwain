1cb08a3092e9c95ee2b9e6fcb7579bf8ab432fda
PedroSillos <pedro.sillos@gmail.com>
2025-09-30 20:18:00 -0300

    ADD A LIST L

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from list_data import list_data
 def main():
     action = input("Enter 'add' to add player or 'list' to show all players: ")
     
-    if action.upper() == "ADD":
+    if action.upper() in ["ADD", "A"]:
         add_player()
         return
-    if action.upper() == "LIST":
+    if action.upper() in ["LIST", "L"]:
         list_data()
         return
     else:
-        print(f"Invalid action: "{action}". Bye.")
+        print(f"Invalid action: '{action}'. Bye.")
 
 if __name__ == "__main__":
     main()
