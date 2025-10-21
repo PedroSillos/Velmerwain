a005f5d7406e9163dc403a9fd9213fbafe8e8564
PedroSillos <pedro.sillos@gmail.com>
2025-10-21 18:52:24 -0300

    Update load_player to load one player at a time

--- src/load_player.py
+++ src/load_player.py
@@ -3,13 +3,19 @@ from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 import requests
 
+def get_input():
+    game_name = input("Enter game name: ")
+    tag_line = input("Enter tag line: ")
+    api_key = getpass.getpass("Enter API key: ")
+    return game_name, tag_line, api_key
+
 def init_spark():
     builder = SparkSession.builder.appName("VelMerWain") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     return configure_spark_with_delta_pip(builder).getOrCreate()
 
-def load_player_bronze(spark, api_key):
+def load_player_bronze(spark, game_name, tag_line, api_key):
     # Define all regions and their super regions
     super_region_map = {
         "americas": ["na1", "br1", "la1", "la2"],
@@ -17,80 +23,54 @@ def load_player_bronze(spark, api_key):
         "europe": ["eun1", "euw1", "me1", "tr1", "ru"],
         "sea": ["oc1", "sg2", "tw2", "vn2"]
     }
-    
+
     # Load existing players to avoid re-fetching
     existing_players = set()
     try:
         existing_player_rows = spark.read.format("delta").load("data/bronze/player").collect()
-        existing_players = {row["puuid"] for row in existing_player_rows}
+        existing_players = {f"{row["gameName"]}#{row["tagLine"]}".upper() for row in existing_player_rows}
     except:
-        print("No player table found")
+        print("\nNo player table found")
+        
+    # If player does not exist, load from 
+    if f"{game_name}#{tag_line}".upper() not in existing_players:
+        print(f"\nPlayer {game_name}#{tag_line} not found in existing players")
 
-    # Empty list that will be filled with new players
-    new_players = []
-    
-    # Load for every region
-    for region in regions:
-        # Determine super region
-        for super_region_i in super_region_map:
-            if region in super_region_map[super_region_i]:
-                super_region = super_region_i
-        
-        # There are too many players, so we limit to only KR region for now
-        if region != "kr":
-            continue
-        
-        # Load from master
-        url = f"https://{region}.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5"
+        url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
         response = requests.get(url, headers={"X-Riot-Token": api_key})
-        
+            
+        while response.status_code == 429:
+            # If rate limit exceeded, wait and retry
+            print("\nRate limit exceeded. Sleeping for 30 seconds...")
+            time.sleep(30)
+            url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+            response = requests.get(url, headers={"X-Riot-Token": api_key})
         if response.status_code == 200:
-            league_data = response.json()
-            for league_entry in league_data["entries"]:
-                if league_entry["puuid"] not in existing_players:
-                    new_players.append({"puuid": league_entry["puuid"]})
-        else:
-            print(f"API Error: {response.status_code}")
-            return
-
-        # Load from grandmaster
-        url = f"https://{region}.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5"
-        response = requests.get(url, headers={"X-Riot-Token": api_key})
-        
-        if response.status_code == 200:
-            league_data = response.json()
-            for league_entry in league_data["entries"]:
-                if league_entry["puuid"] not in existing_players:
-                    new_players.append({"puuid": league_entry["puuid"]})
-        else:
-            print(f"API Error: {response.status_code}")
-            return
-
-        # Load from challenger
-        url = f"https://{region}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5"
-        response = requests.get(url, headers={"X-Riot-Token": api_key})
-        
-        if response.status_code == 200:
-            league_data = response.json()
-            for league_entry in league_data["entries"]:
-                if league_entry["puuid"] not in existing_players:
-                    new_players.append({"puuid": league_entry["puuid"]})
-        else:
-            print(f"API Error: {response.status_code}")
-            return
-        
-        # Save new players to Delta table
-        if new_players:
-            df = spark.createDataFrame(new_players)
+            account_data = response.json()
+            # Save new player to Delta table
+            df = spark.createDataFrame(
+                [{
+                    "gameName": account_data["gameName"],
+                    "tagLine": account_data["tagLine"],
+                    "puuid": account_data["puuid"]
+                }]
+            )
             df.write.format("delta").mode("append").save("data/bronze/player")
-            print(f"\nSaved {len(new_players)} players from {region} region")
+            print(f"\nSaved account data for {account_data["gameName"]}#{account_data["tagLine"]}")
         else:
-            print(f"\nNo new players found from {region}")
+            print(f"\nAPI Error: {response.status_code}")
+            return
+    
+    else:
+        print(f"\nPlayer {game_name}#{tag_line} already exists in the player table")
+
+        #Update existing player info
+
 
 def load_player():
     print("\n ***** Start load players ***** \n")
-    api_key = getpass.getpass("API Key: ")
+    game_name, tag_line, api_key = get_input()
     spark = init_spark()
-    load_player_bronze(spark, api_key)
+    load_player_bronze(spark, game_name, tag_line, api_key)
     spark.stop()
     print("\n ***** End load players ***** \n")
