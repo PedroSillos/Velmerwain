d4f348da1eef5c53b596896e79a9ecb4cd120cc6
PedroSillos <pedro.sillos@gmail.com>
2025-10-28 09:45:34 -0300

    Load match_id by player

--- src/load_match_id.py
+++ src/load_match_id.py
@@ -20,7 +20,7 @@ def load_match_id_bronze(spark, api_key):
         print("\nNo player table found")
         return
 
-    print(f"\nFound {len(players)} new players")
+    print(f"\nFound {len(players)} players")
     
     if not players:
         print("\nNo players found")
@@ -34,37 +34,44 @@ def load_match_id_bronze(spark, api_key):
     except:
         print("\nNo match_id table found")
     
-    '''
     # Counter to know how many players were loaded
     count = 1
     # Empty list to hold new match IDs
     new_match_ids = []
     # Only fetch match IDs for new players
     for player in players:
-        # Fetch match IDs for player from Riot API
-        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{player}/ids?queue=420&type=ranked&start=0&count=100"
-        response = requests.get(url, headers={"X-Riot-Token": api_key})
-        # Check if rate limit was exceeded
-        while response.status_code == 429:
-            # If rate limit exceeded, wait and retry
-            print("Rate limit exceeded. Sleeping for 30 seconds...")
-            time.sleep(30)
-            url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{player}/ids?queue=420&type=ranked&start=0&count=100"
+        print(f"\nStart fetching match_ids for player {count}/{len(players)}")
+        # Initialize to enter the loop
+        match_ids = ["text because cannot be empty"]
+        start = 0
+        while not match_ids == []:
+            # Fetch match IDs for player from Riot API
+            url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{player}/ids?queue=420&type=ranked&start={start}&count=100"
             response = requests.get(url, headers={"X-Riot-Token": api_key})
-        # If successful, load the match IDs
-        if response.status_code == 200:
-            match_ids = response.json()
-            for match_id in match_ids:
-                # Check if match_ids are not already in match_id table
-                if match_id not in existing_match_ids:
-                    new_match_ids.append({"puuid": player, "matchId": match_id})
-                    existing_match_ids.add(match_id)
-        else:
-            print(f"\nAPI Error: {response.status_code}")
-            return
+            # Check if rate limit was exceeded
+            while response.status_code == 429:
+                # If rate limit exceeded, wait and retry
+                print("Rate limit exceeded. Sleeping for 30 seconds...")
+                time.sleep(30)
+                url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{player}/ids?queue=420&type=ranked&start={start}&count=100"
+                response = requests.get(url, headers={"X-Riot-Token": api_key})
+            # If successful, load the match IDs
+            if response.status_code == 200:
+                match_ids = response.json()
+                for match_id in match_ids:
+                    # Check if match_ids are not already in match_id table
+                    if match_id not in existing_match_ids:
+                        new_match_ids.append({"puuid": player, "matchId": match_id})
+                        existing_match_ids.add(match_id)
+            else:
+                print(f"\nAPI Error: {response.status_code} for player {player}")
+                break
+            # Increment start for pagination
+            start += 100
+        print(f"\nFinished fetching match_ids for player {count-1}/{len(players)}")
         # Save new match IDs to match_id table
-        # Save every 30 players to reduce number of writes (but also save at the end)
-        if new_match_ids and (count % 30 == 0 or count == len(players)):
+        # Save every 10 players to reduce number of writes (but also save at the end)
+        if new_match_ids and (count % 10 == 0 or count == len(players)):
             df = spark.createDataFrame(new_match_ids)
             df.write.format("delta").mode("append").save("data/bronze/match_id")
             # Print progress
@@ -73,11 +80,11 @@ def load_match_id_bronze(spark, api_key):
             new_match_ids = []
         # Increment player counter
         count += 1
-    '''
 
 def load_match_id():
     print("\n ***** Start load match_ids ***** \n")
     api_key = getpass.getpass("API Key: ")
+    print("\n")
     spark = init_spark()
     load_match_id_bronze(spark, api_key)
     spark.stop()
